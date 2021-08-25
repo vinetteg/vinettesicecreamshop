11bee8e8a3aa0bfcc5eb3742a857757348dc5c32
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-08-24 21:17:56 -0700

    Adding footer and css elements

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,7 +8,7 @@ import Auth from "../../utils/auth";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
-import { Button } from "semantic-ui-react";
+// import { Button } from "semantic-ui-react";
 
 // stripePromise returns a promise with the stripe object as soon as the Stripe package loads
 const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
