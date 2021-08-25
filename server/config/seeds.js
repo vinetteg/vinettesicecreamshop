d41d6b5baf71fce74cc206c0e68ea5396bc81633
rileyale001 <alexjtriley@gmail.com>
2021-08-24 17:17:18 -0700

    committing on main and alexbackend again

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -23,7 +23,7 @@ db.once('open', async () => {
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
       image: 'banana-pudding-icecream.jpg',
       category: categories[0]._id,
-      price: 2.99,
+      price: 13.99,
       quantity: 500
     },
     {
@@ -32,7 +32,7 @@ db.once('open', async () => {
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
       image: 'strawberry-ice-cream.jpg',
       category: categories[0]._id,
-      price: 1.99,
+      price: 13.99,
       quantity: 500
     },
     {
@@ -41,7 +41,7 @@ db.once('open', async () => {
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
       image: 'vanilla-ice-cream.jpg',
-      price: 7.99,
+      price: 13.99,
       quantity: 20
     },
     {
@@ -50,7 +50,7 @@ db.once('open', async () => {
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
       image: 'strawberry-gelato.jpg',
-      price: 3.99,
+      price: 13.99,
       quantity: 50
     },
     {
@@ -68,7 +68,7 @@ db.once('open', async () => {
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
       image: 'pumpkin-frozen-yogurt.jpg',
-      price: 399.99,
+      price: 13.99,
       quantity: 30
     },
     {
@@ -77,7 +77,7 @@ db.once('open', async () => {
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
       image: 'green-tea-frozen-yogurt.jpg',
-      price: 199.99,
+      price: 18.99,
       quantity: 30
     },
     {
@@ -86,7 +86,7 @@ db.once('open', async () => {
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
       image: 'ice-creamirecipe-book.jpg',
-      price: 9.99,
+      price: 59.99,
       quantity: 100
     },
     {
@@ -94,7 +94,7 @@ db.once('open', async () => {
       category: categories[4]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
       image: 'ice-cream-fanny.jpg',
-      price: 1.99,
+      price: 41.99,
       quantity: 1000
     },
     {
@@ -103,7 +103,7 @@ db.once('open', async () => {
       description:
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
       image: 'scooper-ice.jpg',
-      price: 2.99,
+      price: 22.99,
       quantity: 1000
     },
     {
@@ -112,7 +112,7 @@ db.once('open', async () => {
       description:
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
       image: 'ice-cream-tshirt.jpg',
-      price: 7.99,
+      price: 39.99,
       quantity: 100
     },
     {
@@ -121,7 +121,7 @@ db.once('open', async () => {
       description:
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
       image: 'ice-cream-coffee-cup.jpg',
-      price: 9.99,
+      price: 19.99,
       quantity: 600
     }
   ]);
