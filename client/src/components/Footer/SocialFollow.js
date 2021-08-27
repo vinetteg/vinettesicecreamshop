09497439e2c5708c4cb7ee91050501108045a318
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-08-26 18:36:03 -0700

    Adding npm social package and updating some css

---
+++ client/src/components/Footer/SocialFollow.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function SocialFollow() {
+  return (
+    <div class="social-container">
+      <a
+        href="https://www.youtube.com/c/jamesqquick"
+        className="youtube social"
+      >
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </a>
+      <a
+        href="https://www.facebook.com/learnbuildteach/"
+        className="facebook social"
+      >
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a
+        href="https://www.instagram.com/learnbuildteach"
+        className="instagram social"
+      >
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+    </div>
+  );
+}
