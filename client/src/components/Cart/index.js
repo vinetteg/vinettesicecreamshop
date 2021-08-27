2f9d76c47d3ee1f92bf4f7b7491ac97acd1ef3c7
rileyale001 <alexjtriley@gmail.com>
2021-08-26 19:27:02 -0700

    alexs backend issue

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import { useLazyQuery } from '@apollo/client';
-import { QUERY_CHECKOUT } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import CartItem from '../CartItem';
-import Auth from '../../utils/auth';
-import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
-import './style.css';
-=======
 import React, { useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { useLazyQuery } from "@apollo/client";
@@ -21,7 +9,6 @@ import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
 // import { Button } from "semantic-ui-react";
->>>>>>> 51ea80dc1f9c2e829a3bcf3297ab71b408b64aa8
 
 // stripePromise returns a promise with the stripe object as soon as the Stripe package loads
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
