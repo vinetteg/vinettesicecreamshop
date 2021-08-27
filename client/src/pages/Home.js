57c27ab7de9c3517e37e121b99ee39cdb8fe7be7
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-08-26 18:36:03 -0700

    Adding npm social package and updating some css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
-import Footer from "../components/Footer/footer.js";
+
 // import "/index.css"
 
 const Home = () => {
