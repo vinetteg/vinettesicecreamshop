396f1e3eeb0d798f370d852969dfdf01629baf8c
rileyale001 <alexjtriley@gmail.com>
2021-08-28 13:08:39 -0700

    committing nothing to pull from dev

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -53,7 +53,6 @@ const typeDefs = gql`
     addOrder(products: [ID]!): Order
     updateUser(firstName: String, lastName: String, email: String, password: String): User
     updateProduct(_id: ID!, quantity: Int!): Product
-    addProduct
     login(email: String!, password: String!): Auth
   }
 `;
