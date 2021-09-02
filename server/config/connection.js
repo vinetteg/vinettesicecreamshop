7a966a7f252ab44db2488feb8182bfe275bdcbdf
Matt Koski <mjkekai@gmail.com>
2021-09-01 20:17:47 -0700

    updated database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/safe-island-11025",
+  process.env.MONGODB_URI || "mongodb://localhost/mernshopping",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
